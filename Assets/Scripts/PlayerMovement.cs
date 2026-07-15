b6cf5496c6787b756b6496b90938af1446317406
StarCoder369 <Stargamer.coder@gmail.com>
2026-07-15 12:57:15 -0700

    Basic movement set up
    Basic movement set up

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public InputActionProperty moveAction;
+
+    public float moveSpeed;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnEnable()
+    {
+        moveAction.action?.Enable();
+    }
+
+    void OnDisable()
+    {
+        moveAction.action?.Disable();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 inputVector = moveAction.action.ReadValue<Vector2>();
+
+        Vector3 movementDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        rb.linearVelocity = new Vector3(movementDir.x * moveSpeed, rb.linearVelocity.y, movementDir.z * moveSpeed);
+    }
+}
